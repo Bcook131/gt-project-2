31518bb610b9acd47b12623e3515b2e46614f7ae
Andrew Collier <atc3598@icloud.com>
2020-07-17 11:43:48 -0400

    Delete friends route
    Delete route added for deleting users friends.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,14 +14,12 @@ var db = require("../models");
 module.exports = function(app) {
     
     //GET route for getting all the users
-    app.get("/api/posts", function(req, res) {
+    app.get("/api/user", function(req, res) {
         var query = {};
         if (req.query.author_id) {
           query.AuthorId = req.query.author_id;
         }
-        // Here we add an "include" property to our options in our findAll query
-        // We set the value to an array of the models we want to include in a left outer join
-        // In this case, just db.Author
+        
         db.Post.findAll({
           where: query,
           include: [db.Author]
@@ -30,7 +28,7 @@ module.exports = function(app) {
         });
       });
   // GET route for getting a single user
-  app.get("/api/users", function(req, res) {
+  app.get("/api/user", function(req, res) {
     db.Post.findOne({
         where: {
           id: req.params.id
@@ -42,16 +40,16 @@ module.exports = function(app) {
     });
     
 
-  // POST route for saving a new user. You can create a user using the data on req.body
-  app.post("/api/users", function(req, res) {
+  // POST route for saving a new user. 
+  app.post("/api/user", function(req, res) {
     db.Post.create(req.body).then(function(dbUser) {
         res.json(dbUser);
       });
     });
   
 
-  // DELETE route for deleting users. You can access the user's id in req.params.id
-  app.delete("/api/users/:id", function(req, res) {
+  // DELETE route for deleting users.
+  app.delete("/api/user/:id", function(req, res) {
     db.Post.destroy({
         where: {
           id: req.params.id
@@ -60,8 +58,18 @@ module.exports = function(app) {
         res.json(dbUser);
       });
     });
+    //DELETE route for deleting users' friends. 
+    app.delete("/api/user_friends:id", function(req, res) {
+      db.Post.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(dbUser) {
+          res.json(dbUser);
+        });
+      });
 
-  // PUT route for updating users. The updated user will be available in req.body
+  // PUT route for updating users.
   app.put("/api/users", function(req, res) {
     db.Post.update(
         req.body,
