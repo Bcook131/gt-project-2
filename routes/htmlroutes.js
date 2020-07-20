9a3f5c1aa55cf55009a5aba3377236ddf1c62d8e
binjim2002 <binjim2002@yahoo.fr>
2020-07-20 17:27:48 -0400

    displaying games

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,8 +3,10 @@ module.exports = function(app){
     //load index page
     app.get("/", function(req, res){
         db.Game.findAll().then(games => {
-            console.log(games)
-            res.render("index", {games:games});
+            db.Occupation.findAll().then(occupations => {
+                res.render("index", {games:games,occupations:occupations});
+            })
+            
         })
         
     })
