8d99d780e19daf8acac02e457fa21d7b59e76681
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-20 23:34:16 -0400

    added api routes to get occupations and games arrays

--- public/login.js
+++ public/login.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var passwordInput = $("#password-input");  
     console.log("Hello");
     // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", function(event) {
+    loginForm.on("click", function(event) {
       event.preventDefault();
       
       var userData = {
